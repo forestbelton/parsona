6f39cf6142a5d2dd8e076257cb6a4a99c7f9a4db
Forest Belton <fbelton@theore.tc>
2014-03-02 20:25:35 -0800

    Add calculator test

---
+++ tests/05-calculator.js
@@ -0,0 +1,46 @@
+var P = require('parsona');
+
+var num = P.regex(/[0-9]+/).map(function(s) {
+  return parseInt(s, 10);
+});
+
+var expr = P.lazy(function() {
+  return P.seq([expr, P.string('+'), factor]).map(function(xs) {
+    return xs[0] + xs[2];
+  })
+    .or(P.seq([expr, P.string('-'), factor]).map(function(xs) {
+    return xs[0] - xs[2];
+  }))
+    .or(factor);
+});
+
+var factor = P.lazy(function() {
+  return P.seq([factor, P.string('*'), term]).map(function(xs) {
+    return xs[0] * xs[2];
+  })
+    .or(P.seq([factor, P.string('/'), term]).map(function(xs) {
+    return xs[0] / xs[2];
+  }))
+    .or(term);
+});
+
+var term = P.string('(').then(expr).skip(P.string(')'))
+  .or(num);
+
+exports['2 * 2 = 4'] = function(test) {
+  test.expect(1);
+  test.strictEqual(expr.parse('2*2'), 4);
+  test.done();
+};
+
+exports['1 + 2 * 3 = 7'] = function(test) {
+  test.expect(1);
+  test.strictEqual(expr.parse('1+2*3'), 7);
+  test.done();
+};
+
+exports['(4 / 2) * (3 - 1) = 4'] = function(test) {
+  test.expect(1);
+  test.strictEqual(expr.parse('(4/2)*(3-1)'), 4);
+  test.done();
+};
