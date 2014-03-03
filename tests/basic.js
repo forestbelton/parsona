b4f0c80b9df68c64ee7cd47ceca326f48df1e880
Forest Belton <fbelton@theore.tc>
2014-03-02 19:39:02 -0800

    Split basic tests and method tests

---
+++ tests/basic.js
@@ -0,0 +1,22 @@
+var P = require('parsona');
+
+var a = P.string('a'),
+    b = P.string('b');
+
+exports['regex'] = function(test) {
+  test.expect(1);
+  test.strictEqual(P.regex(/[0-9]+/).parse('123'), '123');
+  test.done();
+};
+
+exports['succeed'] = function(test) {
+  test.expect(1);
+  test.strictEqual(P.succeed(42).parse(''), 42);
+  test.done();
+};
+
+exports['seq'] = function(test) {
+  test.expect(1);
+  test.deepEqual(P.seq([a, b]).parse('ab'), ['a', 'b']);
+  test.done();
+};
